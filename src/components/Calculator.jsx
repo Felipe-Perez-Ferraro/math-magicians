10a415da96a3d7e9d2b8f097bf7a6111a8ba9d87
Feliperto <fperezferraro@gmail.com>
2023-08-10 13:10:21 -0300

    Added useState

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,30 +1,36 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Panel from './Panel';
+import calculate from '../logic/calculate';
 
 /* eslint-disable react/prop-types */
 function Result({ value }) {
   return (
-    <section>
-      <article>
-        <p>{value}</p>
-      </article>
-    </section>
+    <>
+      <span className=" result">{value}</span>
+    </>
   );
 }
 
 function Calculator() {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  const valueHandleClick = (valueClick) => {
-    setValue(valueClick);
+  const handleClick = (buttonName) => {
+    setValue(calculate(value, buttonName));
   };
 
   return (
-    <>
-      <Result value={value} />
-      <Panel valueClick={valueHandleClick} />
-    </>
+    <section className="calculator">
+      <article className="calculator__container">
+        <Result value={value.next || value.total || '0'} />
+        <Panel clickHandle={handleClick} />
+      </article>
+    </section>
   );
 }
 
