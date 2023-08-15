26ecf0ffa1c0caa62cfdf47aa47231cd9e468c08
Feliperto <fperezferraro@gmail.com>
2023-08-15 13:32:26 -0300

    Updated Home.jsx file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+function Home() {
+  return (
+    <section className="home__section">
+      <article className="home__container">
+        <div className="home__features__container">
+          <h1 className="home__title">
+            Welcome to our page!
+          </h1>
+          <p className="home__paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni rerum porro officiis excepturi, est possimus hic, dolore inventore quia et mollitia dolores ad corrupti sed voluptas? Accusantium perspiciatis adipisci, incidunt quis quidem voluptatem ducimus nihil assumenda dolor, velit reiciendis, repellat illo nesciunt facere libero ex totam eum saepe inventore commodi.
+          </p>
+          <p className="home__paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni rerum porro officiis excepturi, est possimus hic, dolore inventore quia et mollitia dolores.
+          </p>
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default Home;
